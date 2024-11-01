03dc09d59a311aad169f7eb1597765c682f02613
juan <juan.saor@mncu.ac.id>
2024-11-01 10:24:03 +0700

    pertemuan5

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,60 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        useMaterial3: false,
-      ),
-      home: const Scaffold(),
+    return const MaterialApp(
+     home: Scaffold(
+       body: Center(
+         child: Image(
+             image:
+             NetworkImage("https://i.pinimg.com/originals/e2/3c/d9/e23cd9aaf6b216fab1c9bc4364284d65.jpg"),
+       ),
+     ),
+    )
     );
   }
 }
+
+  class MyHomePage extends StatefulWidget{
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+    State<MyHomePage> createState() => _MyHomePageState();
+  }
+
+  class _MyHomePageState extends State <MyHomePage> {
+    int _counter = 0;
+
+    void _incerementCounter() {
+      setState(() {
+        _counter++;
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+
+          backgroundColor: Theme
+              .of(context)
+              .colorScheme
+              .inversePrimary,
+
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Text(
+              "Some exciting text",
+            style: TextStyle(color:Colors.red,fontSize: 14),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _incerementCounter,
+          tooltip: 'Increment',
+          child: const Icon (Icons.add),
+        ),
+      );
+    }
+  }
